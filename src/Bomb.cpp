a327923cab91c34a0d8fa24f0ec7dedca8c4cfb6
IftitakhulZakiah <iftitakhulzakiyah@gmail.com>
2017-02-11 22:20:04 +0700

    Bombbbb

--- src/Bomb.cpp
+++ src/Bomb.cpp
@@ -6,13 +6,10 @@ Bomb::Bomb()
 {
 	this->jarak = 0;
 	this->durasi = 1;
-<<<<<<< HEAD
 	Point temp = Point();
 	this->posisi = temp;
-=======
 	this->posisi.SetAbsis(0);
 	this->posisi.SetOrdinat(0);
->>>>>>> d285b4fb42e184cc9cc88b372358b9e9fb39e528
 }
 		
 Bomb::Bomb(int jarak, int durasi, Point posisi)
@@ -27,10 +24,7 @@ Bomb& Bomb::operator= (Bomb& B)
 	this->jarak = B.jarak;
 	this->durasi = B.durasi;
 	this->posisi = B.posisi;
-<<<<<<< HEAD
 	return (*this);
-}
-=======
 }
 int Bomb::GetJarak(){
 	return this->jarak;
@@ -47,4 +41,3 @@ Point Bomb::GetPosisi(){
 void Bomb::SetDuration(int n){
 	this->durasi=n;
 }
->>>>>>> d285b4fb42e184cc9cc88b372358b9e9fb39e528
